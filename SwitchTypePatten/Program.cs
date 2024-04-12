202f0fbe7fd34155735ecf40aa937fa7c344fd77
dell <you@example.com>
2024-04-12 17:47:47 +0530

    Add project files.

---
+++ SwitchTypePatten/Program.cs
@@ -0,0 +1,24 @@
+﻿//Type patten
+int age = 20;
+string name = "Thilina madushan";
+List<string> citys = new List<string> {"Colombo","Galle","Matara" };
+int[] numbers = new int[] {10, 20, 301, 45 };
+double marks = 52.2;
+
+object CheckType(object val) => val switch
+{
+    int => "Integer type",
+    string => "String type",
+    List<string> => "List type",
+    Array => "Array type",
+    _ => "Unknown type",
+};
+
+Console.WriteLine(CheckType(age));
+Console.WriteLine(CheckType(name));
+Console.WriteLine(CheckType(citys));
+Console.WriteLine(CheckType(numbers));
+Console.WriteLine(CheckType(marks));
+
+
+
